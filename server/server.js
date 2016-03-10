fe87bb97662aab14d6f2e7860d645493029f7363
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-09 19:01:43 -0800

    Create server folder

---
+++ server/server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`Hello, world!\n`);
+});
+
+app.listen(PORT);
+console.log(`Running on port ${PORT}`);
