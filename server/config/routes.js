eafe7f567b0a5819914ee0abf6347241c334e739
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-11 12:26:02 -0800

    Require controllers

---
+++ server/config/routes.js
@@ -0,0 +1,10 @@
+var classesController = require('../controllers/classes');
+var classLessonController = require('../controllers/class_lessons');
+var lessonsController = require('../controllers/lessons');
+var receivedResponsesController = require('../controllers/received_responses');
+var requestedResponsesController = require('../controllers/requested_responses');
+var studentsController = require('../controllers/students');
+var studentsClassesController = require('../controllers/students_classes');
+var teacherClassesController = require('../controllers/teacher_classes');
+var teachersController = require('../controllers/teachers');
+
