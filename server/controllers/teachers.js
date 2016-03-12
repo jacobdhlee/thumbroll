d63e7699563e6bf71f6ce0e148416e235f0fb3d2
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-12 15:48:27 -0800

    Create DB query methods for loading lessons, polls

--- server/controllers/teachers.js
+++ server/controllers/teachers.js
@@ -12,16 +12,32 @@ module.exports = {
     //TeacherClasses.getAllClasses(teacherId);
   },
 
+  getLessons: function(req, res, next) {
+    //get all lessons based on req.body.classId
+  },
+
   getPolls: function(req, res, next) {
     // var lessonId = req.body.lessonId;
     // var allPolls = RequestedResponses.getPolls(lessonId);
 
   },
 
+  newPoll: function(req, res, next) {
+    // post new poll to the db with the following object sent over: {lessonId, poll type}
+    // then, after successfully posted to the DB, open up a socket event with a DB-generated
+    // responseId and the poll type for students to send info back.
+  },
+
   thumbsCheck: function(io, req, res, next) {
-    console.log(io);
     // var teacherId = req.body.teacherId;
     // var poll = req.body.poll;
+    io.on('connection', function(client){
+      //client.emit('join');
+      console.log('a teacher connected111!!11!1!!!');
+      setTimeout(function(){
+        io.sockets.emit('thumbsCheck', 'a new thumbsCheck has been opened!');
+      }, 5000);
+    });
 
     // this fake poll would normally be sent by the client to the socket, to the students' clients
     var poll = {
