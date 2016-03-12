4782c4ce0d24502dea19f19ba7d12045e1bf5b18
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-12 15:46:07 -0800

    Remove socket test code, push io object to routes

--- server/app.js
+++ server/app.js
@@ -8,27 +8,6 @@ var PORT = process.env.PORT || 3000;
 
 
 //models.sequelize.sync().then(function () {
-
-  io.on('connection', function(client) {
-    console.log('Someone connected!');
-
-    client.on('join', function(data) {
-      if(data === 'This is your teacher speaking') {
-        client.emit('greeting', 'Hiya, Teach!');
-      } else{
-        client.emit('greeting', 'Hello, student. Please wait for your instructor to open a new poll.');
-      }
-    });
-
-    client.on('newPoll', function(data) {
-      io.sockets.emit('openPoll', data);
-    });
-
-    client.on('disconnect', function(client) {
-      console.log('Bye!\n');
-    });
-  });
-
   require('./config/routes.js')(app, express, io);
 
   server.listen(PORT, function (){
