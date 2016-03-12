708fe9e26e607520ed681207f7e3b6d3229d863a
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-03-11 19:51:23 -0800

    Add routing to feedbackView

--- client/mobile/components/teacher/requestFeedbackView.js
+++ client/mobile/components/teacher/requestFeedbackView.js
@@ -1,4 +1,5 @@
 var React = require('react-native');
+var FeedbackView = require('./display_feedback/feedbackView.js');
 // var api = require('../Utils/api');
 
 var {
@@ -31,24 +32,31 @@ class RequestFeedbackView extends React.Component {
     //populate feedbackOptions with anything custom from lesson
   }
 
-  selectFeebackOption(optionId) {
-    if(optionId === 1) {
-      console.log('Thumbs Check!');
-      //navigate to thumbsCheck
-      //pass feedbackOption options if required
-    } else if(optionId === 2) {
-      console.log('Generic Multiple Choice!');
-      //navigate to multiple choice
-      //pass feedbackOption options if required
-    }
+  dismissClass() {
+    this.props.navigator.pop();
   }
 
-  renderFeedbackOptions(options) {
-    return options.map((option, index) => {
+  selectFeebackOption(feedbackOption) {
+    var component;
+    this.props.navigator.push({
+      component: FeedbackView,
+      userId: this.state.userId,
+      classId: this.state.classId,
+      lessonId: this.state.lessonId,
+      feedbackOption: feedbackOption,
+      sceneConfig: {
+        ...Navigator.SceneConfigs.FloatFromRight,
+        gestures: {}
+      }
+    });
+  }
+
+  renderFeedbackOptions(feedbackOptions) {
+    return feedbackOptions.map((feedbackOption, index) => {
       return (
         <View style={styles.buttonContainer} key={index}>
-          <TouchableOpacity onPress={this.selectFeebackOption.bind(this, option.id)} style={styles.button}>
-            <Text style={styles.buttonText}> {option.name} </Text>
+          <TouchableOpacity onPress={this.selectFeebackOption.bind(this, feedbackOption)} style={styles.button}>
+            <Text style={styles.buttonText}> {feedbackOption.name} </Text>
           </TouchableOpacity>
         </View>
       )
@@ -60,6 +68,11 @@ class RequestFeedbackView extends React.Component {
     return (
       <View style={{flex: 1, backgroundColor: '#ededed'}}> 
         <View style={styles.viewContainer}>
+        <View style={styles.dismissContainer}>
+          <TouchableOpacity onPress={this.dismissClass.bind(this)} style={styles.dismissButton}>
+            <Text style={styles.dismissText}> Dismiss Class </Text>
+          </TouchableOpacity>
+        </View>
           <View style={styles.titleContainer}>
             <Text style={styles.pageText}> Request Feedback: </Text>
           </View>
@@ -73,6 +86,15 @@ class RequestFeedbackView extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  dismissContainer: {
+
+  },
+  dismissButton: {
+
+  },
+  dismissText: {
+
+  },
   viewContainer: {
     flex: 1,
     justifyContent: 'center',
