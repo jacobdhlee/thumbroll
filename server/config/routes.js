d297f54584347667b4bd3ca71e8d3b71f4800548
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-11 19:36:18 -0800

    Remove unnecessary routes

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,28 +1,33 @@
-var classesController = require('../controllers/classes');
-var classLessonController = require('../controllers/class_lessons');
-var lessonsController = require('../controllers/lessons');
-var receivedResponsesController = require('../controllers/received_responses');
-var requestedResponsesController = require('../controllers/requested_responses');
+// var classesController = require('../controllers/classes');
+// var classLessonController = require('../controllers/class_lessons');
+// var lessonsController = require('../controllers/lessons');
+// var receivedResponsesController = require('../controllers/received_responses');
+// var requestedResponsesController = require('../controllers/requested_responses');
 var studentsController = require('../controllers/students');
-var studentsClassesController = require('../controllers/students_classes');
-var teacherClassesController = require('../controllers/teacher_classes');
+// var studentsClassesController = require('../controllers/students_classes');
+// var teacherClassesController = require('../controllers/teacher_classes');
 var teachersController = require('../controllers/teachers');
 var authenticationController = require('../controllers/authenticate');
 
-module.exports = function(app) {
+module.exports = function(app, express) {
 
   app.get('/login', authenticationController.login);
-
   app.get('/signup', authenticationController.signup);
+  
+  app.get('/teachers/poll', teachersController.pollClass);
+  app.get('/teachers/thumbs', teachersController.thumbsCheck);
+  
+  app.post('/teachers/thumbs', teachersController.thumbsCheck);
+  app.post('/teachers/poll', teachersController.pollClass);
 
-  app.get('/teachers', teachersController.getClasses);
+  //app.get('/teachers', teachersController.getAllClasses);
 
   //app.get('/teachers/:classId', teachersController.getOneClass);
 
-  app.get('/students', studentsController.readyStage);
+  //app.get('/students', studentsController.readyStage);
 
-  app.get('/teachers/poll', );
+  //app.get('/classDismissed', teachersController.endClass);
 
-  app.get('/teachers/thumbs', );
+
 
 };
