f893b381ab69173f0c9fdfc8d819230f22b28b35
Donghee Lee <jacobdhlee@gmail.com>
2016-03-11 16:37:10 -0800

    basic structure of join class view

--- client/mobile/components/student/joinClassView.js
+++ client/mobile/components/student/joinClassView.js
@@ -1,1 +1,65 @@
-//joinClassView.js
+var React = require('react-native');
+
+var {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} = React;
+
+class JoinClassView extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      enrolledClasses: ['Quick class','CS101', 'CS201']
+    }
+  }
+
+  selectedClass(cls) {
+    console.log(cls);
+    // this.props.navigator.push({
+
+    // })
+  }
+
+  eachClasses(classes){
+    return classes.map((cls, index) => {
+      return (
+        <View style={styles.buttonsContainer} key={index}>
+          <TouchableOpacity onPress={this.selectedClass.bind(this, cls)}style={styles.buttonContainer}>
+            <Text>{cls}</Text>
+          </TouchableOpacity>
+        </View>
+      )
+    })
+  }
+  render() {
+    return (
+      <View style={{flex: 1, backgroundColor: '#ededed'}}>
+        <View style={styles.textHeader}>
+          <Text style={styles.textSize}>Enrollled Classes</Text>
+        </View>
+        {this.eachClasses(this.state.enrolledClasses)}
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  textHeader: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  textSize: {
+    fontSize: 20
+  },
+  buttonsContainer: {
+    padding: 20
+  },
+  buttonContainer: {
+    margin: 20
+  },
+})
+module.exports = JoinClassView;
