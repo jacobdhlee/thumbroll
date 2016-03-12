6e6cc4fbfa1ad2fda0aec8eaac108fcf67197d70
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-11 16:01:45 -0800

    Create basic socket server

--- server/app.js
+++ server/app.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);  
+var io = require('socket.io').listen(server);
 
 var PORT = 3000;
 
 
 app.get('/', function (req, res) {
-  res.send('Hello, world!');
+  res.sendFile(__dirname + '/socketTest.html');
 })
 
+io.on('connection', function(client) {  
+    console.log('Client connected!');
+    client.on('join', function(data) {
+      console.log(data);
+    });
+    client.on('disconnect', function(client) {
+      console.log('Bye!\n');
+    });
+});
 
-app.listen(PORT, function (){
+
+server.listen(PORT, function (){
   console.log('listening on port', PORT);
 });
