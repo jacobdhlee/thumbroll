7182dd2a0e15dcb9870ed268d56c798d7068066d
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-14 12:20:53 -0700

    Remove express reference, send only app object

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,7 +2,7 @@ var studentsController = require('../controllers/students');
 var teachersController = require('../controllers/teachers');
 var authenticationController = require('../controllers/authenticate');
 
-module.exports = function(app, express, io) {
+module.exports = function(app, io) {
 
   app.get('/login', authenticationController.login);
   app.get('/signup', authenticationController.signup);
@@ -10,7 +10,7 @@ module.exports = function(app, express, io) {
   //app.get('/teachers/poll', teachersController.pollClass);
   //app.get('/teachers/thumbs', teachersController.thumbsCheck);
   
-  app.get('/teachers/thumbs', teachersController.thumbsCheck.bind(this, io));
+  app.get('/teachers/thumbs', teachersController.thumbsCheck(io));
   // app.post('/teachers/poll', teachersController.pollClass);
 
   app.get('/teachers/lessons/:classId', teachersController.getLessons);
@@ -21,5 +21,5 @@ module.exports = function(app, express, io) {
   //app.get('/classDismissed', teachersController.endClass);
   
 
-  app.get('/students/ready', studentsController.readyStage.bind(this, io));
+  app.get('/students/ready', studentsController.readyStage(io));
 };
