227ec4c8bd245fc42e321a61e5b0c454675691e3
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-14 12:19:48 -0700

    Create exports as function taking IO

--- server/controllers/students.js
+++ server/controllers/students.js
@@ -5,8 +5,11 @@
 // var Lessons = require('../models/lessons');
 //var RequestedResponses = require('../models/requested_responses');
 
-module.exports = {
-  readyStage: function(io, req, res, next) {
+module.exports = function(io){
+
+  var functionTable = {};
+
+  functionTable.readyStage = function(req, res, next) {
 
     io.on('connection', function(client){
       console.log('Hey, server! A student is ready to learn!');
@@ -17,14 +20,9 @@ module.exports = {
         io.sockets.emit('responseRecordedFromStudent', data);
       });
 
-      
-
-
     });
-    var responseHTML = `<!doctype html> <html lang='en'> <head> </head> <body> <h1>Thumbroll</h1> <p></p><button id='submit'>Record Response</button><script></script> <script src='https://code.jquery.com/jquery-1.10.2.js'></script> <script src='https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.5/socket.io.js'></script> <script>var socket=io.connect('http://localhost:3000'); socket.on('connect', function(data){socket.emit('studentJoin', 'Hiya, Teach! It's a student!');}); socket.on('thumbsCheck', function(data){console.log('new poll received'); $('p').html(data);}); socket.on('greeting', function(data){console.log(data);}); $('submit').on('click', function(){io.socket.emit('responseRecordedFromStudent');}); </script> </body></html>`;
-
     res.send(responseHTML);
-
-  },
-
+  };
+  
+  return functionTable;
 };
