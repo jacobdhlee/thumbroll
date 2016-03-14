cde3b57d9e5da1480eeab838b5b9860114b0cba3
Donghee Lee <jacobdhlee@gmail.com>
2016-03-14 14:40:58 -0700

    multiChoice works

--- client/mobile/components/student/responses/multiChoice.js
+++ client/mobile/components/student/responses/multiChoice.js
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     flex: .1, 
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FF3366'
+    backgroundColor: '#FF3366',
   },
   button:{
     flex: .2,
