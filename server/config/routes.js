73aed179803919cd5e6297178844ae7c7058eae9
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-14 17:17:12 -0700

    Change thumbsCheck and poll route to generic poll route

--- server/config/routes.js
+++ server/config/routes.js
@@ -7,13 +7,10 @@ module.exports = function(app, io) {
   app.get('/login', authenticationController.login);
   app.get('/signup', authenticationController.signup);
     
-  app.get('/teachers/thumbs', teachersController.thumbsCheck);
+  app.post('/teachers/poll', teachersController.pollClass.bind(null, io));
 
   app.get('/teachers/lessons/:classId', teachersController.getLessons);
-  app.get('/teachers/polls/:lessonId', teachersController.getPolls);
-
-  app.post('/teachers/polls', teachersController.newPoll);
-  
+  app.get('/teachers/polls/:lessonId', teachersController.getPolls);  
 
   app.get('/students/ready', studentsController.readyStage.bind(null, io));
 };
