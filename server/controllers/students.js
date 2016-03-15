70476681ff261c6d8bd979eb7fe1879cc0062c39
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-15 11:40:26 -0700

    Migrate student socket logic to socketLogic file

--- server/controllers/students.js
+++ server/controllers/students.js
@@ -9,27 +9,10 @@ module.exports = {
 
   readyStage : function(io, req, res, next) {
 
-    //var studentInformation = req.body.studentData
-    // var pollResponse = {
-    //   responseId: 1,
-    //   type: 'thumbs',
-    //   datetime: new Date(),
-    //   lessonId: 13,
-    // };
-
     io.on('connection', function(student){
-      
       student.emit('studentStandby', studentInformation);
-
-      student.on('newPoll', function(data) {
-        student.emit(data);
-      });
-
-      // setTimeout(function(){
-      //   io.sockets.emit('responseFromStudent', pollResponse);
-      // }, 5000);
-
     });
+
     res.status(200).send('Hello from the student side');
   }
 };
--- server/socketLogic.js
+++ server/socketLogic.js
@@ -11,14 +11,11 @@ module.exports = function(io) {
 
     //TEACHER CODE
       
-    client.on('studentStandby', function(data) {
+    client.on('studentConnect', function(data) {
       // FRONTEND-LISTENER: client.on('newStudentConnected', (studentInfo) => {display(studentCount++ and studentInfo);});
-      client.emit('newStudentConnected', data);
+      
+      //TODO: DB.write('newStudentConnected', data);
     });
-
-    // setTimeout(function(){
-    //   io.sockets.emit('newPoll', poll);
-    // }, 5000);
 
     client.on('responseFromStudent', function(data) {
       // write data to the DB
@@ -28,7 +25,6 @@ module.exports = function(io) {
       // FRONTEND-LISTENER: client.on('studentData', (data) => {display(data)});
       client.emit('studentData', data);
     });
-    //res.status(201).send('Hello from the client side');
 
     client.on('teacherConnect', function(data) {
       console.log('Teacher connected');
@@ -37,10 +33,6 @@ module.exports = function(io) {
 
     //STUDENT CODE
 
-    client.on('studentConnect', function(data) {
-      console.log('Student connected');
-    });
-
     client.on('newPoll', function(data) {
       client.emit(data);
     });
@@ -48,7 +40,6 @@ module.exports = function(io) {
     setTimeout(function(){
       io.sockets.emit('responseFromStudent');
     }, 5000);
-
 
   });
 };
