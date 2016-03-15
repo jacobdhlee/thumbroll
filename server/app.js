814e87b6042f8bac5bb9d64773068610c1709406
Jake Pace <jakeyrob@gmail.com>
2016-03-15 13:51:38 -0700

    Add additional sample users to classes

--- server/app.js
+++ server/app.js
@@ -45,6 +45,13 @@ models.students.create({
                     });
 
 
+models.students.create({
+                      firstname: 'Orlando',
+                      lastname: 'Calrisian',
+                      username: 'OlandoC',
+                      password: 'pass123',
+                    });
+
 models.classes.create({
                       name: 'CS101',
                       teacher_id: 1,
@@ -73,8 +80,10 @@ models.students_classes.create({
                       student_id: 2
                     });
 
-
-
+models.students_classes.create({
+                      class_id: 2,
+                      student_id: 3
+                    });
 
 
 
