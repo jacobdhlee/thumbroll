d5a1a3c5b0683a5a226f03159f38aea15357b486
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-14 19:45:24 -0700

    Require socketLogic

--- server/app.js
+++ server/app.js
@@ -2,12 +2,25 @@ var app = require('express')();
 var server = require('http').createServer(app);  
 var io = require('socket.io').listen(server);
 var models = require("./models");
+var socketLogic = require("./socketLogic");
+var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 models.sequelize.sync().then(function () {
   require('./config/routes.js')(app, io);
+
+  //deploy socketLogic
+
+  socketLogic(io);
+
+
+
+
 
   server.listen(PORT, function (){
     console.log('listening on port', PORT);
