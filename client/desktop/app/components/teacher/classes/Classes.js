8ba380f2adc76cd6afe9c61cb91821e39596f1ee
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-21 14:48:40 -0700

    Display mock class data, rename

---
+++ client/desktop/app/components/teacher/classes/Classes.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+class Classes extends React.Component {
+  // constructor(props){
+  //   super(props);
+    
+  //   this.state = {
+  //     //state here
+  //   };
+  // }
+
+  render(){
+    return (
+      <div>
+        <h1>Classes</h1>
+        <div>
+          <button>Add new class</button>
+        </div>
+        <li>CS 101</li>
+        <li>CS 201</li>
+        <li>HIST 302</li>
+      </div>
+    );
+  }
+
+  addClass(){
+
+  }
+
+  
+}
+
+module.exports = Classes;
