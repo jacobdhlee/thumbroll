c302dedaa00153f6417171c33481455266e72e75
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-21 14:49:26 -0700

    Build Lesson class structure

---
+++ client/desktop/app/components/teacher/classes/lessons/lessonData.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class LessonData extends React.Component {
+  // constructor(props){
+  //   super(props);
+    
+  //   this.state = {
+  //     //state here
+  //   };
+  // }
+
+  render(){
+    return <li>CS 101</li>
+  }
+}
+
+module.exports = LessonData;
