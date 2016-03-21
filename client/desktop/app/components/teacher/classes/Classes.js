32682924b98f57fc803b9c9b13d760140fcbc95e
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-21 16:44:34 -0700

    Add class to page

--- client/desktop/app/components/teacher/classes/Classes.js
+++ client/desktop/app/components/teacher/classes/Classes.js
@@ -1,33 +1,43 @@
 import React from 'react'
 
 class Classes extends React.Component {
-  // constructor(props){
-  //   super(props);
+  constructor(props){
+    super(props);
     
-  //   this.state = {
-  //     //state here
-  //   };
-  // }
+    this.state = {
+      classes : this.props.teacherData.map((specificClass) => {
+        return <li key={specificClass.name}>{specificClass.name}</li>
+      }),
+      newClassName : ''
+    };
+  }
 
   render(){
     return (
       <div>
         <h1>Classes</h1>
         <div>
-          <button>Add new class</button>
+          <input type='text' onChange={(event) => this.setState({newClassName: event.target.value})} />
+          <button onClick={this.addClass.bind(this)}>Add new class</button>
         </div>
-        <li>CS 101</li>
-        <li>CS 201</li>
-        <li>HIST 302</li>
+        {this.state.classes}
       </div>
     );
   }
 
   addClass(){
+    // update state
+    var classesCopy = this.state.classes.slice();
+    classesCopy.push(<li key={this.state.newClassName}>{this.state.newClassName}</li>);
+    this.setState({
+      classes: classesCopy
+    });
 
+
+    // post to DB
   }
 
-  
+
 }
 
 module.exports = Classes;
