3f7ccc4880e60bacc03a71dc1170f3b22c128214
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-03-22 11:42:48 -0700

    Fix bug causing teacher view to crash on thumbscheck

--- client/mobile/components/student/responses/thumbRoll.js
+++ client/mobile/components/student/responses/thumbRoll.js
@@ -83,6 +83,7 @@ class ThumbRoll extends React.Component {
         this.setState({
           value: percent
         });
+        this.props.onUpdate(Math.floor(percent * 100));
       },
       onPanResponderRelease: (evt, gestureState) => {
         // The user has released all touches while this view is the
@@ -98,10 +99,6 @@ class ThumbRoll extends React.Component {
         return true;
       },
     });
-  }
-
-  valueChange(value) {
-    this.setState({value: Math.floor(value)})
   }
   
   render() {
