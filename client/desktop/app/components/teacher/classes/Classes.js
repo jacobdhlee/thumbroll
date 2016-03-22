26202540269e8dbd1da636e7009790065db401e4
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-22 16:07:23 -0700

    Store and pass display to children

--- client/desktop/app/components/teacher/classes/Classes.js
+++ client/desktop/app/components/teacher/classes/Classes.js
@@ -7,36 +7,61 @@ class Classes extends React.Component {
     super(props);
     
     this.state = {
+      display: this.props.display,
+      displayListener: this.props.displayListener,
       classes : this.props.teacherData.map((specificClass) => {
-        return <li style={{cursor: 'default'}} onClickCapture={(event) => this.setState({currentClass: event.target.innerText})} key={specificClass.name}>{specificClass.name}</li>
+        return (<li style={{cursor: 'default'}} onClickCapture={(event) => {
+          this.setState({
+            currentClass: event.target.innerText,
+          });
+          this.state.displayListener('lessons');
+        }} key={specificClass.name}>{specificClass.name}</li>)
       }),
       newClassName : '',
       currentClass: ''
     };
   }
 
+  checkDisplaySettings(){
+    if(this.props.display !== this.state.display) {
+      this.setState({
+        display: this.props.display
+      });
+    }
+  }
+
   render(){
-    return (
-      <div>
-        <h2>Classes</h2>
-        {this.state.classes}
+    this.checkDisplaySettings();
+    console.log('state: ', this.state.display, 'props', this.props.display)
+    if(this.state.display === 'home'){
+      return (
         <div>
-          <form onSubmit={this.addClass.bind(this)}>
-            <input className='newClassForm' type='text' value={this.state.newClassName} onChange={(event) => {
-              this.setState({
-                newClassName: event.target.value
-              });
-            }} />
-            <div>
-              <button type='submit'>Add new class</button>
-            </div>
-          </form>
-        </div>
+          <h2>Classes</h2>
+          {this.state.classes}
+          <div>
+            <form onSubmit={this.addClass.bind(this)}>
+              <input className='newClassForm' type='text' value={this.state.newClassName} onChange={(event) => {
+                this.setState({
+                  newClassName: event.target.value
+                });
+              }} />
+              <div>
+                <button type='submit'>Add new class</button>
+              </div>
+            </form>
+          </div>
         <div>
-          <Lessons className={this.state.currentClass}/>
+          <Lessons display={this.state.display} displayListener={this.state.displayListener.bind(this)} className={this.state.currentClass}/>
         </div>
       </div>
     );
+   } else {
+      return (
+        <div>
+          <Lessons display={this.state.display} displayListener={this.state.displayListener.bind(this)} className={this.state.currentClass}/>
+        </div>
+      )
+    }
   }
 
   addClass(e){
