cb4eb35adba8a953a16a3beec7453059e4d94171
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-22 11:45:08 -0700

    Show poll buttons only displaying lesson data

--- client/desktop/app/components/teacher/classes/lessons/lessonData.js
+++ client/desktop/app/components/teacher/classes/lessons/lessonData.js
@@ -22,23 +22,23 @@ class LessonData extends React.Component {
       <li style={this.props.className === '' ? {display:'none'} : {}}>Little Bobby Tables</li>
       <li style={this.props.className === '' ? {display:'none'} : {}}>Suzie CSS</li>
       <li style={this.props.className === '' ? {display:'none'} : {}}>Edison</li>
-      <AddPoll />
+      <AddPoll hide={!this.props.className}/>
     </div>
   }
 
 
 }
 
-const AddPoll = () => {
+const AddPoll = (hide) => {
   // add to state
 
   // post to DB
 
   return (
     <div>
-      <h2>Add new poll</h2>
-      <button>Thumbs Check</button>
-      <button>Multiple Choice</button>
+      <h2 style={hide === true ? {display:'none'} : {}}>Add new poll</h2>
+      <button style={hide === true ? {display:'none'} : {}}>Thumbs Check</button>
+      <button style={hide === true ? {display:'none'} : {}}>Multiple Choice</button>
     </div>
   )
 };
