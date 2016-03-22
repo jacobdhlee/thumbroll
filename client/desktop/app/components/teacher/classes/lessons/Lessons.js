3d94e66e263ac307f0b9e7a7ae864edd41f52fa1
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-22 11:33:04 -0700

    Display today's lesson independent of which component is loaded

--- client/desktop/app/components/teacher/classes/lessons/Lessons.js
+++ client/desktop/app/components/teacher/classes/lessons/Lessons.js
@@ -17,8 +17,8 @@ class Lessons extends React.Component {
   render(){
     return (
       <div>
-        <h2 style={this.props.className === '' ? {display:'none'} : {}}>Today's Lessons</h2>
-        <p style={this.props.className === '' ? {display:'none'} : {}}>There are no lessons today.</p>
+        <h2>Today's Lessons</h2>
+        <p>There are no lessons today.</p>
         <LessonData className={this.props.className}/>
       </div>
     )
