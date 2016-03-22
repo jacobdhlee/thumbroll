be4880069876c20072e47e8a3d71c16eb5b1ac88
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-21 20:13:18 -0700

    Create LessonsData to house all polls for specific lessons

--- client/desktop/app/components/teacher/classes/lessons/lessonData.js
+++ client/desktop/app/components/teacher/classes/lessons/lessonData.js
@@ -1,17 +1,48 @@
 import React from 'react'
 
 class LessonData extends React.Component {
-  // constructor(props){
-  //   super(props);
+  constructor(props){
+    super(props);
     
-  //   this.state = {
-  //     //state here
-  //   };
-  // }
+    this.state = {
+      // state here
+      // lessons data
+      // polls
+      newPollSelected: false
+    };
+  }
 
   render(){
-    return <li>CS 101</li>
+    return <div>
+      <h2>{this.props.className}</h2>
+      <p style={this.props.className === '' ? {display:'none'} : {}}>Polls for {this.props.className}</p>
+      <li style={this.props.className === '' ? {display:'none'} : {}}>January 1, 1970</li>
+      <li style={this.props.className === '' ? {display:'none'} : {}}>March 31, 2034</li>
+
+      <p style={this.props.className === '' ? {display:'none'} : {}}>Students in {this.props.className}</p>
+      <li style={this.props.className === '' ? {display:'none'} : {}}>Little Bobby Tables</li>
+      <li style={this.props.className === '' ? {display:'none'} : {}}>Suzie CSS</li>
+      <li style={this.props.className === '' ? {display:'none'} : {}}>Edison</li>
+      <AddPoll newPollSelected={this.state.newPollSelected}/>
+    </div>
   }
+
+
 }
+
+const AddPoll = () => {
+  // add to state
+
+  // post to DB
+  
+  return (
+    <div>
+      <h2>Add new poll</h2>
+      <button>Thumbs Check</button>
+      <button>Multiple Choice</button>
+    </div>
+  )
+};
+
 
 module.exports = LessonData;
