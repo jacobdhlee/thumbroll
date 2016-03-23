8fd885f431d74fe3adf0af96693c4ffe385f7c22
Jake Pace <jakeyrob@gmail.com>
2016-03-23 11:49:16 -0700

    Implement protected routes and logout route

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,23 +1,20 @@
 var studentsController = require('../controllers/students');
 var teachersController = require('../controllers/teachers');
 var authenticationController = require('../controllers/authenticate');
-var path = require('path');
+var utils = require('../utils/utils');
 
 module.exports = function(app, io) {
 
   app.post('/login', authenticationController.login);
   app.post('/signup', authenticationController.signup);
+  app.get('/logout', authenticationController.logout);
     
-  app.post('/teachers/polls/', teachersController.pollClass.bind(null, io));
+  app.post(utils.checkUser, '/teachers/polls/', teachersController.pollClass.bind(null, io));
 
-  app.get('/teachers/polls/:lessonId', teachersController.getLessonPolls);  
+  app.get(utils.checkUser, '/teachers/polls/:lessonId', teachersController.getLessonPolls);  
   
-  app.get('/teachers/lessons/:classId', teachersController.getClassLessons);
-  app.post('/teachers/lessons', teachersController.addClassLesson);
+  app.get(utils.checkUser, '/teachers/lessons/:classId', teachersController.getClassLessons);
+  app.post(utils.checkUser, '/teachers/lessons', teachersController.addClassLesson);
 
-  app.post('/students/ready', studentsController.readyStage.bind(null, io));
-
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../../client/desktop/dist', 'index.html'));
-  })
+  app.post(utils.checkUser, '/students/ready', studentsController.readyStage.bind(null, io));
 };
--- server/utils/utils.js
+++ server/utils/utils.js
@@ -10,5 +10,6 @@ exports.createSession = function(req, res, newUser) {
 
 exports.checkUser = function(req, res, next) {
   // Set property on body indicating whether user has a session
-  res.body.loggedIn = exports.isLoggedIn(req);
+  res.body.hasNoSession = !exports.isLoggedIn(req);
+  res.status(401).send('user is not logged in');
 };
