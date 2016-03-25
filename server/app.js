f298bb155b67a833f629c1366a44f241a5e8c45d
Jake Pace <jakeyrob@gmail.com>
2016-03-25 13:20:34 -0700

    Fix CORS handling for localhost:3000

--- server/app.js
+++ server/app.js
@@ -15,10 +15,13 @@ var PORT = process.env.PORT || 3000;
 
 // CORS handling
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
+  
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
