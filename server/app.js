3f0c3cb575763e90800f3feff233f5aee27f8b30
Shane McGraw <shanemcgraw17@gmail.com>
2016-03-31 17:32:28 -0700

    Change CORS headers to thumbroll.xyz

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ var PORT = process.env.PORT || 80;
 
 // CORS handling
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://45.55.204.109:80"); 
+  res.header("Access-Control-Allow-Origin", "http://thumbroll.xyz"); 
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Credentials", true);
