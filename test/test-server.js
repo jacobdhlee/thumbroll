cde36af5b2de79c83078f1a6e7cea4605c3392ce
Donghee Lee <jacobdhlee@gmail.com>
2016-04-05 09:02:18 -0700

    change test to chai with chai-http

--- test/test-server.js
+++ test/test-server.js
@@ -1,9 +1,100 @@
+// var chai = require('chai');
+// var server = 'http://localhost:3000';
+// var expect = require('chai').expect;
+// var app = require('../server/app')
+// var supertest = require('supertest');
+// var api = supertest(app)
+
 var chai = require('chai');
+var chaiHttp = require('chai-http');
 var server = 'http://localhost:3000';
 var expect = require('chai').expect;
-var app = require('../server/app')
-var supertest = require('supertest');
-var api = supertest(app)
+
+chai.use(chaiHttp);
+
+describe('Thumbroll', function(){
+  it('should list all classes for teacher id 1', function(done){
+    chai.request(server)
+    .get('/teachers/classes/1')
+    .end(function(err,res){
+      expect(res).to.have.status(200);
+      expect(res.body[2].name).to.deep.equal("History")
+      expect(err).to.be.null;
+      done();
+    })
+  })
+})
+
+describe('Thumbroll', function(){
+  it('should list all lessons for class id 4', function(done){
+    chai.request(server)
+    .get('/classes/lessons/4')
+    .end(function(err,res){
+      expect(res).to.have.status(200);
+      expect(res.lesson).to.deep.equal();
+      expect(err).to.be.null;
+      done();
+    })
+  })
+})
+
+describe('Thumbroll', function(){
+  it('should list of poll for lesson id 2', function(done){
+    chai.request(server)
+    .get('/lessons/2/polls')
+    .end(function(err,res){
+      expect(res).to.have.status(200);
+      expect(res.body[0].type).to.deep.equal('thumbs');
+      expect(err).to.be.null;
+      done();
+    })
+  })
+})
+
+describe('Thumbroll', function(){
+  it('should list class for student id 1', function(done){
+    chai.request(server)
+    .get('/students/classes/1')
+    .end(function(err,res){
+      var classes = res.body[0].class
+      expect(res).to.have.status(200);
+      expect(classes.name).to.deep.equal('CS101');
+      expect(err).to.be.null;
+      done();
+    })
+  })
+})
+// teachers/polls/:lessonId
+
+describe('Thumbroll', function(){
+  it('should list class for student id 1', function(done){
+    chai.request(server)
+    .get('/teachers/polls/3')
+    .end(function(err,res){
+      expect(res).to.have.status(200);
+      expect(res.body[0].type).to.deep.equal('multiChoice');
+      expect(res.body[1].type).to.deep.equal('thumbs');
+      expect(err).to.be.null;
+      done();
+    })
+  })
+})
+
+describe('Thumbroll', function(){
+  it('should list lesson for class id 3', function(done){
+    chai.request(server)
+    .get('/teachers/classes/3/lessons')
+    .end(function(err,res){
+      expect(res).to.have.status(200);
+      expect(res.body[0].name).to.deep.equal('Week 1 - Ancient Egypt');
+      expect(res.body[1].name).to.deep.equal('Week 2 - Mongols');
+      expect(err).to.be.null;
+      done();
+    })
+  })
+})
+
+
 
 describe('Thumbroll', function(){
   it('should list all classes for teacher id 1', function(done){
@@ -12,7 +103,7 @@ describe('Thumbroll', function(){
     .end(function(err,res){
       expect(200)
       expect(function(res){
-        res.body[2].name = "Postmodern Gender Theory";
+        res.body[2].name === "Postmodern Gender Theory";
       })
       done();
     })
@@ -25,7 +116,7 @@ describe('Thumbroll', function(){
     .end(function(err,res){
       expect(200)
       expect(function(res){
-        res.lesson = ''
+        res.lesson === 'akfhakshdfk'
       })
       done();
     })
